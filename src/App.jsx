87703aae53f1418b70c8f4afb2078004ac173030
franciscorodriguezsv24 <franciscorodriguezsv24@gmail.com>
2024-06-27 13:33:32 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import AppCss from './App.module.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className={AppCss.header}>Hola</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
