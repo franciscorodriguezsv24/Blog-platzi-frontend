7af1bc0fc045a2ba60a18a5edad5da24c886e61d
franciscorodriguezsv24 <franciscorodriguezsv24@gmail.com>
2024-07-06 20:15:27 -0600

    Navabar in web and mobile way

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,20 +1,45 @@
 import StyleCss from './NavBar.module.css'
 import Unkwon from '../../../public/Unknown_person.jpg'
+import googleFonts from 'google-fonts';
+import Logo from '../../../public/blogger-logo-icon-png-2.png'
+import { FaUserFriends, FaHome, FaUser, FaSearch} from 'react-icons/fa'
+
+
+
+googleFonts.add({
+  'Roboto': true,
+  'Lato': [400, 700], // Ejemplo con múltiples pesos
+});
+
 
 const NavBar = () => {
 
   return(
     <>
       <nav className={StyleCss.container}>
+        <div className={StyleCss.searchContainer}>
+          <div className={StyleCss.containerLogo}>
+            <img src={Logo} alt="Logo" className={StyleCss.searchLogo}/>
+          </div>
+          <input className={StyleCss.searchInput} type="text" placeholder="Search.."/>
+          <button className={StyleCss.searchButton}><FaSearch size={25}/></button>
+        </div>
         <ul className={StyleCss.listContainer}>
-          <a>About You</a>
-          <a>Profile</a>
-          <a>Dashboard</a>
-          <a>Friends</a>
+        <div>
+          <FaHome size={65}/>
+        </div>
+        <div>
+          <FaUserFriends size={65}/>  
+        </div>
+        <div>
+          <FaUser size={55}/>
+        </div>
+          
         </ul>
-
-        <div className={StyleCss.profileImgContainer}>
-          <img src={Unkwon} alt="profile-img" className={StyleCss.profileImg}/>
+        <div className={StyleCss.profileContainer}>
+          <div className={StyleCss.profileImgContainer}>
+            <img src={Unkwon} alt="profile-img" className={StyleCss.profileImg}/>
+          </div>
         </div>
       </nav>
     </>
